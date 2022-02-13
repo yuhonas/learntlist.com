e8afee3ec5812e62e8c48a7b540567f09106cb82
Clint Plummer <clint@yuhonas.com>
2022-02-13 13:40:44 +1100

    add qasar + basic learnt item card component

---
+++ src/components/LearntItems.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="flex">
+    <div class="row">
+      <q-card class="my-card" flat bordered v-for="item in items" :key="item.url">
+        <q-card-section>
+          {{ item.url }}
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'LearntItems',
+  data() {
+    return {
+      items: [
+        { url: 'http://www.google.com' },
+        { url: 'http://www.example.com' },
+      ]
+    }
+  }
+}
+</script>
